49d369023bbaba2345c1332f7c5f66707cddcc33
AarhamH <ahaider@cayenta.com>
2024-08-22 00:47:06 -0700

    Added basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import "./App.css"
 import {ColorModeProvider} from "@kobalte/core"
 import { SideNavigation } from "./components/SideNavigation";
+import { Route, Router } from "@solidjs/router";
+import { HomePage } from "./pages/HomePage";
 
 export default function App() {
   return (
     <ColorModeProvider initialColorMode="dark">
-      <SideNavigation/>
+      <div class="flex flex-row">
+        <div class="flex-shrink-0">
+          <SideNavigation/>
+        </div>
+        <div class="flex-1">
+          <Router>
+            <Route path="/" component={HomePage} />
+          </Router>
+        </div>
+      </div>
     </ColorModeProvider>
   );
 }
