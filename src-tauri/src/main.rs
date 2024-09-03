fc4acef7d9698e72ba358fa63786f995f692d70f
AarhamH <ahaider@cayenta.com>
2024-09-02 23:53:15 -0700

    Deleted entities module

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-mod db;
+pub mod db;
+pub mod models;
+pub mod schema;
+
+pub mod operations {
+    pub mod playlist_operations;
+    pub mod music_operations;
+}
+
 fn main() {
     tauri::Builder::default()
         .setup(|_app| {
