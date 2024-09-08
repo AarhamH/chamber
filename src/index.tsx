c444c404f19c862033c2b9264f9bec31125279cc
AarhamH <ahaider@cayenta.com>
2024-09-08 16:38:02 -0700

    Updated routing strat...again

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,38 @@
 /* @refresh reload */
+import "./App.css"
 import { render } from "solid-js/web";
-import App from "./App";
+import { Route, Router } from "@solidjs/router"
+import { SideNavigation } from "./components/SideNavigation";
+import { HomePage } from "./pages/HomePage";
+import { SearchPage } from "./pages/SearchPage";
+import {ColorModeProvider} from "@kobalte/core"
+import { JSX } from "solid-js";
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+type AppProps = {
+  children?: JSX.Element;
+};
+
+const App = (props: AppProps) => {
+  return (
+    <>
+      <ColorModeProvider initialColorMode="dark">
+        <div class="flex flex-row">
+          <div class="flex-shrink-0">
+            <SideNavigation />
+          </div>
+          <div class="flex-1">
+            {props.children}
+          </div>
+        </div>
+      </ColorModeProvider>
+    </>
+  );
+}
+
+render(
+  () => (
+  <Router root={App}>
+    <Route path="/" component={HomePage} />
+    <Route path="/search" component={SearchPage} />
+  </Router>), 
+  document.getElementById("root") as HTMLElement);
