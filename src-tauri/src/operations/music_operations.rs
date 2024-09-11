a71030575f0ef3bee1d1d617fe1b5a195ecb0d43
AarhamH <ahaider@cayenta.com>
2024-09-10 23:53:41 -0700

    Added error handling for music operations

--- src-tauri/src/operations/music_operations.rs
+++ src-tauri/src/operations/music_operations.rs
@@ -7,7 +7,7 @@ use crate::models::music_model:: {
 use crate::db::establish_connection;
 
 #[tauri::command]
-pub fn create_music(music_arg: MusicArg) {
+pub fn create_music(music_arg: MusicArg) -> Result<(), String> {
   use crate::schema::music::dsl::*;
 
   let mut connection: SqliteConnection = establish_connection();
@@ -16,19 +16,31 @@ pub fn create_music(music_arg: MusicArg) {
     title: &music_arg.title.unwrap_or_default().to_string(),
     artist: &music_arg.artist.unwrap_or_default().to_string(),
     path: &music_arg.path.unwrap_or_default().to_string(),
+    duration: &music_arg.duration.unwrap_or_default().to_string()
   };
 
-  diesel::insert_into(music)
+  let result: Result<usize, diesel::result::Error> = diesel::insert_into(music)
     .values(&new_music)
-    .execute(&mut connection)
-    .expect("Error saving new music");
+    .execute(&mut connection);
+
+  match result {
+    Ok(_) => Ok(()),
+
+    Err(diesel::result::Error::DatabaseError(diesel::result::DatabaseErrorKind::UniqueViolation, _)) => {
+        Err("Error: Could not add music entry to database".to_string()) // Return error to the client
+    }
+    Err(err) => {
+        Err(format!("Error: {}", err))
+    }
+  }
+
 }
 
 #[tauri::command]
-pub fn update_music(id_arg: i32, music_arg: MusicArg) {
+pub fn update_music(id_arg: i32, music_arg: MusicArg) -> Result<(), String> {
   use crate::schema::music::dsl::*;
 
-  let mut connection = establish_connection();
+  let mut connection: SqliteConnection = establish_connection();
 
     let current_song: Music = music
         .find(id_arg)
@@ -40,12 +52,16 @@ pub fn update_music(id_arg: i32, music_arg: MusicArg) {
         title: music_arg.title.unwrap_or(current_song.title),
         artist: music_arg.artist.unwrap_or(current_song.artist),
         path: music_arg.path.unwrap_or(current_song.path),
+        duration: music_arg.duration.unwrap_or(current_song.duration)
     };
+ 
+    let result: Result<_, _> = diesel::update(music.find(id_arg))
+      .set(&new_music)
+      .execute(&mut connection);
 
-    diesel::update(music.find(id_arg))
-        .set(&new_music)
-        .execute(&mut connection)
-        .expect("Error updating playlist");
-
-
+    match result {
+      Ok(_) => Ok(()),
+      Err(err) => Err(format!("Error updating music entry: {}", err)), // Return error to the client
+    }
+  
   }
