dd47f3a7288e7df65c67af57f26393ec0403e63f
AarhamH <ahaider@cayenta.com>
2024-09-11 00:20:31 -0700

    Added music getAll and music getFromPlaylist queries

--- src-tauri/src/operations/music_operations.rs
+++ src-tauri/src/operations/music_operations.rs
@@ -3,7 +3,6 @@ use diesel::prelude::*;
 use crate::models::music_model:: {
     Music, MusicArg, NewMusic
 };
-
 use crate::db::establish_connection;
 
 #[tauri::command]
@@ -33,8 +32,58 @@ pub fn create_music(music_arg: MusicArg) -> Result<(), String> {
         Err(format!("Error: {}", err))
     }
   }
-
 }
+
+#[tauri::command]
+pub fn get_all_music() -> Result<Vec<Music>, String> {
+  use crate::schema::music::dsl::*;
+
+  let mut connection: SqliteConnection = establish_connection();
+
+  let music_list: Vec<Music> = match music.load::<Music>(&mut connection) {
+      Ok(result) => result,
+      Err(err) => {
+          eprintln!("Error loading playlists: {}", err);
+          return Err(format!("Error querying playlist entries: {}", err)); // Return an error message
+      }
+  };
+  
+  Ok(music_list)
+}
+
+#[tauri::command]
+pub fn get_all_music_from_playlist(playlist_id_arg: i32) -> Result<Vec<Music>, String> {
+  use crate::schema::playlist_music::dsl::*;
+  use crate::schema::music::dsl::*;
+
+  let mut connection: SqliteConnection = establish_connection();
+
+  let music_id_list: Vec<i32> = match playlist_music
+  .filter(playlist_id.eq(playlist_id_arg))
+  .select(music_id) 
+  .load::<i32>(&mut connection) {
+      Ok(ids) => ids,
+      Err(err) => {
+        eprintln!("Error loading music IDs from playlist: {}", err);
+        return Err(format!("Error querying playlist music entries: {}", err));
+      }
+  };
+
+  let music_list: Vec<Music> = match music
+  .filter(id.eq_any(music_id_list)) // Filter by the list of music_ids
+  .load::<Music>(&mut connection) {
+      Ok(result) => result,
+      Err(err) => {
+          eprintln!("Error loading music: {}", err);
+          return Err(format!("Error querying music: {}", err));
+      }
+  };
+
+  Ok(music_list)
+  
+}
+
+
 
 #[tauri::command]
 pub fn update_music(id_arg: i32, music_arg: MusicArg) -> Result<(), String> {
