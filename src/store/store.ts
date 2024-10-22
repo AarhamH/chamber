849ef8bff7b01f8980173a951e52c94fcb205de2
AarhamH <ahaider@cayenta.com>
2024-10-21 21:14:23 -0700

    Added global store for audio queue

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,3 +6,4 @@ export const [music, setMusic] = createStore<Music[]>([]);
 export const [musicInPlaylist, setMusicInPlaylist] = createStore<Music[]>([]);
 export const [activeAudio, setActiveAudio] = createStore<Music>({} as Music);
 export const [youtubeQueue, setYoutubeQueue] = createStore<YoutubeQuery[]>([]);
+export const [audioCodecQueue, setAudioCodecQueue] = createStore<string[]>([]);
