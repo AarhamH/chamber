1b1233dcc6578adebab61fadba57bb8bc8b6403d
AarhamH <aarham.haider@gmail.com>
2024-10-22 22:45:06 -0400

    Use all audio modal component in playlist

--- src/pages/PlaylistPage.tsx
+++ src/pages/PlaylistPage.tsx
@@ -1,6 +1,5 @@
 import { useNavigate, useParams } from "@solidjs/router"
 import { invoke } from "@tauri-apps/api/tauri";
-import { open } from "@tauri-apps/api/dialog";
 import { createEffect, createSignal} from "solid-js";
 import { Playlist, PlaylistArg } from "~/utils/types";
 import {
@@ -20,16 +19,17 @@ import {
 } from "~/components/Dropdown"
 
 import { Music } from "~/utils/types";
-import { playlists, setPlaylists, musicInPlaylist, setMusicInPlaylist, music, setMusic, activeAudio } from "~/store/store";
+import { playlists, setPlaylists, musicInPlaylist, setMusicInPlaylist, activeAudio } from "~/store/store";
 import { Button } from "~/components/Button";
 import img from "~/assets/GOJIRA-THE-WAY-OF-ALL-FLESH-2XWINYL-2627680470.png";
 import { BiRegularPause, BiRegularPlay } from "solid-icons/bi"
 import { BiRegularDotsVerticalRounded } from "solid-icons/bi"
 import { IoAdd } from "solid-icons/io"
 import { useAudio } from "~/components/AudioContext";
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "~/components/Dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "~/components/Dialog";
 import { toast } from "solid-sonner";
 import CustomToast from "~/components/CustomToast";
+import { AllAudioModal } from "~/components/table/AllAudioModal";
 
 export const PlaylistPage = () => {
   const params = useParams();
@@ -41,7 +41,6 @@ export const PlaylistPage = () => {
   createEffect(() => {
     if (params.id) {
       fetchMusicFromPlaylist();
-      fetchAllAudio();
       const playlist = playlists.find((playlistItem) => playlistItem.id === parseInt(params.id));
       setPlaylistTitle(playlist?.title || "");
     }
@@ -51,15 +50,6 @@ export const PlaylistPage = () => {
     try {
       const result = await invoke<Music[]>("get_all_music_from_playlist", {playlistIdArg: parseInt(params.id)});
       setMusicInPlaylist(result);
-    } catch (error) {
-      return error
-    }
-  }
-
-  const fetchAllAudio = async () => {
-    try {
-      const result = await invoke<Music[]>("get_all_music");
-      setMusic(result);
     } catch (error) {
       return error
     }
@@ -85,29 +75,12 @@ export const PlaylistPage = () => {
     try{
       await invoke("insert_song_into_playlist", { playlistIdArg: parseInt(params.id), musicIdArg: id });
       fetchMusicFromPlaylist();
+      return `Successfully added to playlist: ${playlistTitle()}`;
     } catch (error) {
       return new Error(String(error));
     }
   };
 
-  const addAudio= async () => {
-    const filePaths = await open({
-      multiple: true,
-      filters: [{
-        name: "Audio Files",
-        extensions: ["mp3", "wav"],
-      }],
-    });
-    
-    // Check if any files were selected
-    if (filePaths && Array.isArray(filePaths)) {
-      for (const filePath of filePaths) {
-        await invoke("create_music", { filePath });
-      }
-      fetchAllAudio();
-    }
-  };
- 
   const deleteCurrentPlaylist = async () => {
     try {
       await invoke("delete_playlist", { playlistIdArg: parseInt(params.id) });
@@ -145,51 +118,7 @@ export const PlaylistPage = () => {
             {/* Modals for Add Audio/Delete Group */}
             <Dialog>
               <DialogTrigger class="w-32" as={Button} variant={"filled"} size={"sm"}>Add Audio</DialogTrigger>
-              <DialogContent class="w-5/6 h-2/3">
-                <DialogHeader>
-                  <div class="flex flex-col items-center justify-center">
-                    <DialogTitle>Add to Playlist</DialogTitle>
-                    <Button class="w-32" onClick={addAudio} variant={"link"}>Add Audio</Button>      
-                  </div>
-                  <DialogDescription>
-                    <Table>
-                      <TableHeader>
-                        <TableRow>
-                          <TableHead class="w-2 truncate">ID</TableHead>
-                          <TableHead class="w-1/4 truncate">Title</TableHead>
-                          <TableHead class="w-2/12 truncate">Artist</TableHead>
-                          <TableHead class="w-1/2 truncate">Path</TableHead>
-                          <TableHead class="w-10 truncate">Duration</TableHead>
-                          <TableHead class="w-16 text-right truncate"></TableHead>
-                        </TableRow>
-                      </TableHeader>
-                      <TableBody>
-                        {music.map((song:Music,index:number) => (
-                          <TableRow>
-                            <TableCell class="max-w-sm truncate overflow-hidden whitespace-nowrap">{index+1}</TableCell>
-                            <TableCell class="max-w-sm truncate overflow-hidden whitespace-nowrap">{song.title}</TableCell>
-                            <TableCell class="max-w-sm truncate overflow-hidden whitespace-nowrap">{song.artist}</TableCell>
-                            <TableCell class="max-w-sm truncate overflow-hidden whitespace-nowrap">{song.path}</TableCell>
-                            <TableCell class="max-w-sm truncate overflow-hidden whitespace-nowrap">{song.duration}</TableCell>
-                            <TableCell>
-                              <IoAdd 
-                                class="flex justify-end hover:cursor-pointer" 
-                                size={"1.5em"} 
-                                onClick={() => {
-                                  insertMusicToPlaylist(song.id).then(result => {
-                                    const isError = result instanceof Error;
-                                    (() => isError ? toast.error(result.message) : toast.success(`Successfully added to playlist: ${playlistTitle()}`))();
-                                  });
-                                }}
-                              />
-                            </TableCell>  
-                          </TableRow>
-                        ))}
-                      </TableBody>
-                    </Table>
-                  </DialogDescription>
-                </DialogHeader>
-              </DialogContent>
+              <AllAudioModal title="Add to playlist" modalAction={{icon: IoAdd, onClick: insertMusicToPlaylist}} />
             </Dialog>
             <Dialog>
               <DialogTrigger class="w-32" as={Button} variant={"destructive"} size={"sm"}>Delete Playlist</DialogTrigger>
