111b826d0d646200c556d7ae09299b431e3caa07
AarhamH <aarham.haider@gmail.com>
2024-10-22 22:45:29 -0400

    Grid in codec page

--- src/pages/Encoding.tsx
+++ src/pages/Encoding.tsx
@@ -2,10 +2,17 @@ import { Button } from "~/components/Button"
 import { open } from "@tauri-apps/api/dialog";
 import { Dialog, DialogTrigger } from "~/components/Dialog";
 import { AllAudioModal } from "~/components/table/AllAudioModal";
+import { audioCodecQueue, setAudioCodecQueue, music } from "~/store/store";
+import { Music } from "~/utils/types";
 import { IoAdd } from "solid-icons/io";
-
+import { Table, TableBody, TableCell, TableRow } from "~/components/Table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "~/components/Dropdown"
 export const Encoding = () => {
-
   const addAudio= async () => {
     const filePaths = await open({
       multiple: true,
@@ -16,18 +23,64 @@ export const Encoding = () => {
     });
   };
 
+  const insertFromAllAudios = async (id:number) => {
+    try{
+      setAudioCodecQueue([...audioCodecQueue, ...music.filter((audio: Music) => audio.id === id)]);
+      return "Successfully added to transcoding queue";
+    } catch (error) {
+      return new Error(String(error));
+    }
+  };
+
   return(
-    <Dialog>
-      <div class="w-full h-full flex items-center justify-center">
+    <div class="w-full h-full flex items-center justify-center">
+      {audioCodecQueue.length <= 0 ? (
         <div class="flex flex-col items-center justify-center">
           <div class="text-3xl font-thin">Transcoding Queue Empty</div>
           <div class="flex gap-5">
             <Button class="mt-5 w-32 border-2 border-zinc-600 hover:bg-transparent hover:border-opacity-60" size={"sm"} onClick={addAudio}>From Machine</Button>
-            <DialogTrigger as={Button} class="mt-5 w-32 border-2 border-zinc-600 hover:bg-transparent hover:border-opacity-60" size={"sm"}>All Audios</DialogTrigger>
-            <AllAudioModal title="Add to Transcoding" modalAction={{icon:IoAdd, onClick:()=>{}}} />
+            <Dialog>
+              <DialogTrigger as={Button} class="mt-5 w-32 border-2 border-zinc-600 hover:bg-transparent hover:border-opacity-60" size={"sm"}>All Audios</DialogTrigger>
+              <AllAudioModal title="Add to Transcoding" modalAction={{icon:IoAdd, onClick:insertFromAllAudios}} />
+            </Dialog>
           </div>
         </div>
-      </div>
-    </Dialog>
+      ):(
+        <div>
+          <div class="flex items-center justify-center gap-5 pb-5">
+            <Button class="mt-5 w-32 border-2 border-zinc-600 hover:bg-transparent hover:border-opacity-60" size={"sm"} onClick={addAudio}>From Machine</Button> <Dialog>
+              <DialogTrigger as={Button} class="mt-5 w-32 border-2 border-zinc-600 hover:bg-transparent hover:border-opacity-60" size={"sm"}>All Audios</DialogTrigger>
+              <AllAudioModal title="Add to Transcoding" modalAction={{icon:IoAdd, onClick:insertFromAllAudios}} />
+            </Dialog>
+          </div>
+          <div class="max-h-96 border border-zinc-900 rounded-lg overflow-y-auto">
+            <Table>
+              <TableBody>
+                {audioCodecQueue.map((audio: Music) => (
+                  <TableRow>
+                    <TableCell class="max-w-sm">{audio.title}</TableCell>
+                    <TableCell class="max-w-sm ">{audio.path}</TableCell>
+                    <TableCell>
+                      <DropdownMenu>
+                        <DropdownMenuTrigger as={Button} variant={"link"}>
+                          Mp3
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                          <DropdownMenuItem 
+                            class="text-red-500 hover:cursor-pointer"
+                          >
+                            Delete
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>                    
+                    </TableCell>  
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      )}
+    </div>
   )
 }
