6e60459333132262a52d35d3097a536916fa636b
AarhamH <ahaider@cayenta.com>
2024-10-27 16:02:15 -0700

    Read file in chunks

--- src-tauri/src/commands/audio_buffer.rs
+++ src-tauri/src/commands/audio_buffer.rs
@@ -6,8 +6,14 @@ pub fn read_audio_buffer(file_path: String) -> Result<String, String> {
         .map_err(|e| e.to_string())?;
     
     let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer)
-        .map_err(|e| e.to_string())?;
+    let mut buffer_container = vec![0; 128 * 1024];
+
+    while let Ok(bytes_read) = file.read(&mut buffer_container) {
+        if bytes_read == 0 {
+            break;
+        }
+        buffer.extend_from_slice(&buffer_container[..bytes_read]);
+    }
     
     // Encode the buffer as a base64 string
     let encoded = base64::encode(buffer);
