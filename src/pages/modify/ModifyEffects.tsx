123531dfe0ff782bfae9002409b5fe4c1dbf1625
AarhamH <aarham.haider@gmail.com>
2024-11-11 05:41:58 -0500

    Implemented recording page

--- src/pages/modify/ModifyEffects.tsx
+++ src/pages/modify/ModifyEffects.tsx
@@ -1,58 +1,140 @@
-import { Button } from "~/components/Button"
-import { Dialog, DialogTrigger } from "~/components/Dialog";
-import { AllAudioModal } from "~/components/table/AllAudioModal";
-import { audioCodecQueue, setAudioCodecQueue, audio, isAudioTranscodeLoading, setIsAudioTranscodeLoading, setEffectInput } from "~/store/store";
-import { Audio, AudioCodec } from "~/utils/types";
-import { IoAdd } from "solid-icons/io";
-import { Table, TableBody, TableCell, TableRow } from "~/components/Table";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger
-} from "~/components/Dropdown"
-import { AiOutlineMinusCircle } from "solid-icons/ai";
-import { BiRegularLoaderCircle } from "solid-icons/bi";
-import { toast } from "solid-sonner";
-import { SUPPORTED_TYPES } from "~/utils/constants";
-import { effectInput } from "~/store/store";
+import { useColorMode } from "@kobalte/core";
+import { createSignal, onMount, createEffect } from "solid-js";
+import WaveSurfer from "wavesurfer.js";
+import RecordPlugin from "wavesurfer.js/dist/plugins/record.js";
+import { Button } from "~/components/Button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "~/components/Dropdown";
 
 export const ModifyEffects = () => {
-  const supportedAudioTypes = SUPPORTED_TYPES
+  let container!: HTMLDivElement;
+  let wavesurfer!: WaveSurfer;
+  let record!: RecordPlugin;
+  const [micList, setMicList] = createSignal<MediaDeviceInfo[]>([]);
+  const [selectedMic, setSelectedMic] = createSignal<MediaDeviceInfo>();
+  const [audioList, setAudioList] = createSignal<Blob[]>([]);
+  const [isRecording, setIsRecording] = createSignal(false);
+  const [isPaused, setIsPaused] = createSignal(false);
+  const [time, setTime] = createSignal(0);
+  const { colorMode } = useColorMode();
 
-  const insertFromAllAudios = async (id: number) => {
-    try {
-      const foundAudio = audio.find((audio_item: Audio) => audio_item.id === id);
-      if (foundAudio) {
-        setEffectInput(foundAudio);
-      } else {
-        throw new Error("Audio not found");
-      }
-      return "Successfully added to transcoding queue";
-    } catch (error) {
-      return new Error(String(error));
+  onMount(() => {
+    wavesurfer = WaveSurfer.create({
+      container: container,
+      waveColor: "violet",
+      barWidth: 3,
+      barRadius: 3,
+      height: 100,
+    });
+
+    record = wavesurfer.registerPlugin(RecordPlugin.create({
+      scrollingWaveform: false,
+      renderRecordedAudio: false,
+    }));
+  });
+
+  createEffect(() => {
+    record.on("record-end", (blob: Blob) => {
+      setAudioList([...audioList(), blob]);
+    });
+  })
+
+  const recordAudio = () => {
+    if (record.isPaused() || record.isRecording()) {
+      record.stopRecording();
+      return;
     }
+    record.startRecording({deviceId: "default"})
   };
 
-  const addToList = (id: number, isAddedToList: boolean) => {
-    setAudioCodecQueue(audioCodecQueue.map(audio_item => 
-      audio_item.id === id ? { ...audio_item, is_added_to_list: isAddedToList } : audio_item
-    ));
+  const pauseRecording = () => {
+    if(record.isPaused()) {
+      record.resumeRecording();
+      return;
+    }
+    record.pauseRecording();
   }
 
-  return(
-    <div class="flex items-center justify-center h-full">
-      {Object.keys(effectInput).length === 0 ? (
-        <div class="flex flex-col items-center justify-center">
-          <div class="text-3xl font-thin">Welcome to the Effects Pannel</div>
-          <Dialog>
-            <DialogTrigger as={Button} class="mt-5 w-32 border-2 hover:bg-transparent hover:border-opacity-60" size={"sm"}>(+) Add to queue</DialogTrigger>
-            <AllAudioModal title="Add to Transcoding" modalAction={{icon:IoAdd, onClick:insertFromAllAudios}} />
-          </Dialog>
+  createEffect(() => {
+    if (wavesurfer) {
+      wavesurfer.setOptions({
+        waveColor: colorMode() == "dark" ? "white" : "black",
+      });
+    } else {
+      return new Error(String("WaveSurfer instance is not initialized"));
+    }
+  });
+
+
+  createEffect(() => {
+    record.on("record-start", () => {
+      setIsRecording(true);
+    })
+    record.on("record-end", () => {
+      setTime(0);
+      setIsRecording(false);
+    })
+    record.on("record-pause", () => {
+      setIsPaused(true);
+    })
+    record.on("record-resume", () => {
+      setIsPaused(false);
+    });
+    record.on("record-progress", (duration: number) => {
+      setTime(duration);
+    });
+  });
+
+  createEffect(() => {
+    RecordPlugin.getAvailableAudioDevices().then((list: MediaDeviceInfo[]) => {
+      setMicList(list);
+    });
+  },[micList]);
+
+  const formatTime = (duration: number) => {
+    const minutes = Math.floor(duration / 60000);
+    const seconds = Math.floor((duration % 60000) / 1000);
+    const milliseconds = Math.floor((duration % 1000) / 10); // Keep milliseconds to 2 significant figures
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}:${milliseconds < 10 ? "0" : ""}${milliseconds}`;
+  };
+
+  return (
+    <div>
+      <div class="flex flex-col items-center justify-center">
+        <div class="pt-10 text-4xl font-thin">Recorder</div>
+        <div class="text-2xl font-thin">{formatTime(time())}</div>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger as={Button} class="w-fit pl-2 pr-2" size={"sm"}>
+            {selectedMic()?.label || "Select Mic"}
+          </DropdownMenuTrigger>
+          <DropdownMenuContent >
+            {micList().map((mic: MediaDeviceInfo) => (
+              <DropdownMenuItem onClick={() => setSelectedMic(mic)}>{mic.label}</DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>                    
+      </div>
+      <div class="p-10" ref={container}></div>
+      <div class="flex flex-col items-center justify-center">
+        <div>
+          <Button class="w-32" size={"sm"} onClick={recordAudio}>{isRecording() ? "Stop Recording" : "Record"}</Button>
+          <Button class="w-32" size={"sm"} onClick={pauseRecording}>{isPaused() ? "Resume" : "Pause"}</Button>
         </div>
-      ):(
-        <div>Items are here!</div>
-      )} 
+      </div>
+      <div class="flex flex-col items-center overflow-auto h-64 ml-20 mr-20 border-2 border-secondary">
+        {
+          audioList().map((audio: Blob,index) => (
+            <div class="flex flex-row items-center">
+              <span>{index+1}</span>
+              <audio controls src={URL.createObjectURL(audio)} />
+              <Button class="w-32" size={"sm"} variant={"link"} onClick={() => 
+              {setAudioList(audioList().filter((_, i) => i !== index))}}>
+                Delete
+              </Button>
+            </div>
+          ))
+        }
+      </div>
     </div>
-  )
-}
+  );
+};
